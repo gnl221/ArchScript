0b334b6302bd260a6f8910f81dffba83e42011dd
Greg Lehto <gnl221@gmail.com>
2024-04-20 10:37:47 -0400

    Update kderice-restore.sh
    Add konsave to aur pkg instead of pip

--- scripts/kderice-restore.sh
+++ scripts/kderice-restore.sh
@@ -2,7 +2,6 @@
 
 export PATH=$PATH:~/.local/bin
 cp -r $HOME/$SCRIPTHOME/configs/.config/* $HOME/.config/
-pip install konsave
 konsave -i $HOME/$SCRIPTHOME/configs/kde.knsv
 sleep 1
 konsave -a kde
